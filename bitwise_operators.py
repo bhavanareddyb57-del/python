99bf38891ed1da82b763e147245b46abb22e5ef2
bhavanareddyb57-del <bhavanareddyb57@gmail.com>
2026-09-25 09:08:09 +0530

    Add files via upload

---
+++ bitwise_operators.py
@@ -0,0 +1,29 @@
+#bitwise_operators 
+a = 5
+b = 3
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<b)
+print(a>>b)
+
+
+#electric  city bill calculator
+units = int(input("enter electricity units:"))
+
+rate  = 6
+
+bill=units*rate
+
+print("eleltricity bill:",bill)
+
+#travel expense calculator
+travel =float(input("travel expense:"))
+food =float(input("food expense:")) 
+hotel=float(input("hotel expense:"))
+
+total=travel+food+hotel
+print("total expense:",total)
+
+
+
