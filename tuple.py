d1584d54864de978ab7270185612ee5c1422fde9
bhavanareddyb57-del <bhavanareddyb57@gmail.com>
2026-09-25 09:14:26 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,22 @@
+#tupple in python
+#tupples is a collection of multiple valuesthat is odered and can t be changed
+#tuple is immutable
+
+student=("bhavana",98,"python")
+print(student[0])
+#access values in a tuple
+students=("bhavana",18,90,5)
+
+print(student[0])
+print(student[1])
+print(student[2])
+
+#tuples are  immutable meaning they cannot be changed after
+
+numbers=(10,20,20,30,30)
+
+numbers=(numbers.count)
+
+
+numbers=(10,20,30,40)
+print(numbers.index(30))
